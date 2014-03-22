17d5f67ca97d4309497fff81c6241d72526d0575
Khurram Virani <kvirani@gmail.com>
2014-03-22 09:52:30 -0700

    add form post action to allow message submission

--- app/actions.rb
+++ app/actions.rb
@@ -11,3 +11,13 @@ end
 get '/messages/new' do
   erb :'messages/new'
 end
+
+post '/messages' do
+  @message = Message.new(
+    title:   params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
