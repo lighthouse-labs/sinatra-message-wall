52a28c96c6b0a916fce496516894ea6d601417d7
Khurram Virani <kvirani@gmail.com>
2014-03-24 10:50:19 -0700

    Added helper current_user to cleanup and DRY up the code

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,12 @@
 # Homepage (Root path)
+
+helpers do
+  # Usable in ERB templates everywhere, and in any action below
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
 get '/' do
   erb :index
 end
